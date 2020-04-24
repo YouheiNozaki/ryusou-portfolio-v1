94a4026aee3f50b04baa10e47f4de8486a81480a
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-04-24 13:20:29 +0900

    setting datafetching of microcms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,42 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Typescript Starter`,
+    title: `Ryusou Tech Blog`,
+    description: `りゅーそうの技術ブログ&ポートフォリオサイト`,
+    author: `Yohei Nozaki`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: ` Ryusou Blog`,
+        short_name: `Ryusou Blog`,
+        start_url: `/`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-graphql-codegen`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-source-microcms',
+      options: {
+        apiKey: process.env.X_API_KEY,
+        serviceId: 'ryusou-mtkh',
+        endpoint: 'posts',
+      },
+    },
   ],
-}
+};
