ddb93bce2f39ac0a988e13b4b0f5b7b9a43dd9bd
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-04-29 23:18:05 +0900

    gatsby-nodeを設定

---
+++ gatsby-node/index.ts
@@ -0,0 +1,51 @@
+import path from 'path';
+import { GatsbyNode } from 'gatsby';
+import {
+  MicrocmsPostsConnection,
+  MicrocmsPosts,
+} from '../graphql-types';
+
+type Result = {
+  allMicrocmsPosts: MicrocmsPostsConnection;
+};
+
+export type PostContext = {
+  post: MicrocmsPosts;
+};
+
+const query = `
+{
+  microcmsPosts {
+    postsId
+    title
+    tags {
+      name
+    }
+    day
+    image {
+      url
+    }
+    content
+  }
+}
+`;
+
+export const createPages: GatsbyNode['createPages'] = async ({
+  graphql,
+  actions: { createPage },
+}) => {
+  const result = await graphql<Result>(query);
+  const { edges } = result.data?.allMicrocmsPosts;
+
+  const postTemplate = path.resolve(
+    './src/templates/post.tsx',
+  );
+
+  edges?.forEach((edge) => {
+    createPage<PostContext>({
+      path: `/posts/${edge.node.postsId}`,
+      component: postTemplate,
+      context: { post: edge.node },
+    });
+  });
+};
---
+++ src/templates/post.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { PostContext } from '../../gatsby-node';
+
+type Props = {
+  pageContext: PostContext;
+};
+
+const Post: React.FC<Props> = ({ pageContext }) => {
+  const post = pageContext.post;
+  return (
+    <>
+      <h1>{post.title}</h1>
+      {post?.tags?.map(
+        (tag) =>
+          tag?.id && (
+            <React.Fragment key={tag.id}>
+              <span>{tag.name}</span>
+            </React.Fragment>
+          ),
+      )}
+      <p>{post.day}</p>
+      <img src={post.image.url} />
+      <p
+        dangerouslySetInnerHTML={{
+          __html: `${post.content}`,
+        }}
+      ></p>
+    </>
+  );
+};
+
+export default Post;
