98d23a5c3b581bd4ba4a08304f40b9cddb7cb336
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-05-19 00:22:18 +0900

    huskyの設定

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = () => ({
   plugins: [
     require('tailwindcss')(`./tailwind.config.js`),
     require(`autoprefixer`),
+    require(`cssnano`)({
+      preset: `default`,
+    }),
   ],
 });
