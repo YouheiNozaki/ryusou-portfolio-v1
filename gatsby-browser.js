3705e77a07361554390db5c4c679dec3106ed81b
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-06-02 11:44:39 +0900

    gatsby-browser.jsをwrapPageElementに変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from './src/components/layout';
 
-export const wrapRootElement = ({ element }) => {
+export const wrapPageElement = ({ element }) => {
   return <Layout>{element}</Layout>;
 };
