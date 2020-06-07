59105bb17c0a499661b42e030ed447c579373d6d
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-06-07 17:01:05 +0900

    headerの基本的なスタイル

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -2,6 +2,38 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
+import { css } from '@emotion/core';
+
+import { colors, sizes, typography } from '../../../theme/';
+
+const HeaderContainer = css({
+  display: 'flex',
+  alignItems: 'center',
+  padding: sizes[5],
+});
+
+const HeaderLogo = css({
+  marginRight: 'auto',
+});
+
+const HeaderLink = css({
+  display: 'flex',
+  margin: '0 0 0 auto',
+  alignItems: 'center',
+  '& li': {
+    margin: '0 0 0 auto',
+    marginRight: sizes[8],
+    listStyle: 'none',
+    '& a': {
+      color: colors.blue,
+      textDecoration: 'none',
+      fontWeight: typography.fontWeights.bold,
+    },
+    '& a:hover': {
+      color: colors.red,
+    },
+  },
+});
 
 export const Header: React.FC = () => {
   const data = useStaticQuery(graphql`
@@ -18,15 +50,29 @@ export const Header: React.FC = () => {
 
   return (
     <>
-      <Link to="/">
-        <Img
-          fixed={data.file.childImageSharp.fixed}
-          alt="Logo"
-        />
-      </Link>
-      <Link to="/posts">POST</Link>
-      <Link to="/works">WORK</Link>
-      <Link to="/contacts">CONTACT</Link>
+      <div css={HeaderContainer}>
+        <div css={HeaderLogo}>
+          <Link to="/">
+            <Img
+              fixed={data.file.childImageSharp.fixed}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        <nav>
+          <ul css={HeaderLink}>
+            <li>
+              <Link to="/posts">POST</Link>
+            </li>
+            <li>
+              <Link to="/works">WORK</Link>
+            </li>
+            <li>
+              <Link to="/contacts">CONTACT</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 };
