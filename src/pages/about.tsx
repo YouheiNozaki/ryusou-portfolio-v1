3c7a1e5c04d6de7b8b8a01777c4c7586d0d694bc
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-06-10 23:00:17 +0900

    aboutページを作成

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 import Image from 'gatsby-image';
 import { FluidImage } from '../lib/images';
 import { css } from '@emotion/core';
-import { color } from 'csx';
+import { color, repeatingLinearGradient } from 'csx';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
 import {
@@ -64,8 +64,8 @@ const Career = css({
   '& table': {
     margin: `${sizes[16]} auto`,
     width: sizes.largeSizes.xl,
-    borderTop: `solid ${sizes[1]} ${colors.lightBlue}`,
-    borderBottom: `solid ${sizes[1]} ${colors.lightBlue}`,
+    borderTop: `solid ${sizes[1]} ${colors.blue}`,
+    borderBottom: `solid ${sizes[1]} ${colors.blue}`,
     borderCollapse: 'collapse',
     '& caption': {
       fontSize: fontSizes.lg,
@@ -73,13 +73,13 @@ const Career = css({
       fontWeight: typography.fontWeights.black,
     },
     '& thead': {
-      background: `${color(colors.lightBlue).fade(0.8)}`,
+      background: `${color(colors.blue).fade(0.6)}`,
       '& tr th': {
         color: colors.white,
         fontWeight: typography.fontWeights.black,
         paddingLeft: sizes[12],
         borderBottom: `dashed ${sizes[1]} ${color(
-          colors.lightBlue,
+          colors.blue,
         ).fade(0.4)}`,
       },
     },
@@ -89,8 +89,39 @@ const Career = css({
         fontWeight: typography.fontWeights.black,
         paddingLeft: sizes[8],
         borderBottom: `dashed ${sizes[1]} ${color(
-          colors.lightBlue,
+          colors.blue,
         ).fade(0.4)}`,
+      },
+    },
+  },
+});
+
+const SkillStack = css({
+  margin: `${sizes[16]} auto`,
+  '& h2': {
+    textAlign: 'center',
+  },
+  '& .SkillStackList': {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    '& .SkillStackItem': {
+      margin: sizes[8],
+      display: 'flex',
+      '& .SkillStackItemContent': {
+        marginLeft: sizes[2],
+        '& h3': {
+          color: colors.white,
+          backgroundColor: colors.red,
+          marginTop: sizes[1],
+          marginBottom: sizes[1],
+          padding: sizes[2],
+          borderRadius: sizes[2],
+        },
+        '& p': {
+          color: colors.blue,
+          marginTop: sizes[1],
+          marginLeft: sizes[2],
+        },
       },
     },
   },
@@ -150,28 +181,36 @@ const About: React.FC<Props> = ({ data }) => (
             </tbody>
           </table>
         </section>
-        <section>
+        <section css={SkillStack}>
           <h2>技術スタック</h2>
-          {data?.allMicrocmsSkills?.edges.map((edge) => {
-            const skill = edge.node;
-            return (
-              <React.Fragment key={skill.id}>
-                <div>
-                  {skill?.title &&
-                    skill?.fields?.featuredImage?.fixed && (
-                      <Image
-                        fixed={
-                          skill.fields.featuredImage.fixed
-                        }
-                        alt={skill.title}
-                      />
-                    )}
-                  <h3>{skill.title}</h3>
-                  <p>{skill.parameter}</p>
-                </div>
-              </React.Fragment>
-            );
-          })}
+          <div className="SkillStackList">
+            {data?.allMicrocmsSkills?.edges.map((edge) => {
+              const skill = edge.node;
+              return (
+                <React.Fragment key={skill.id}>
+                  <div className="SkillStackItem">
+                    {skill?.title &&
+                      skill?.fields?.featuredImage
+                        ?.fixed && (
+                        <Image
+                          fixed={
+                            skill.fields.featuredImage.fixed
+                          }
+                          alt={skill.title}
+                        />
+                      )}
+                    <div className="SkillStackItemContent">
+                      <h3>{skill.title}</h3>
+                      <p>{skill.parameter}</p>
+                    </div>
+                  </div>
+                </React.Fragment>
+              );
+            })}
+          </div>
+          <p>
+            ※この数値は妄想であり、現実の能力とは解離している場合があります。
+          </p>
         </section>
       </article>
     </section>
