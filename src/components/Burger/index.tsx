7b5b011c54c468013538f5cf5f5477691c89bad6
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-06-16 20:44:49 +0900

    ハンバーガーメニューのスタイルを変更,スイッチの切り替えを実装

--- src/components/Burger/index.tsx
+++ src/components/Burger/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/core';
 import { sizes, colors } from '../../theme';
 
@@ -20,17 +20,16 @@ const hamburger = css({
     width: '48px',
     height: '48px',
     borderRadius: '50%',
-    border: '1px solid #f9d8ae',
-    boxShadow: '0 0 2rem transparent',
+    border: `${sizes[1]} solid ${colors.blue}`,
     outline: 'none',
     WebkitTransition: 'all .3s ease-in-out',
     transition: 'all .3s ease-in-out',
   },
   '& .hamburger:hover': {
-    boxShadow: '0 0 .5rem rgba(255, 255, 255, .5 )',
+    boxShadow: `0 0 ${sizes.px} ${colors.blue}`,
   },
   '& .hamburger:focus': {
-    boxShadow: `0 0 ${sizes[2]} ${colors.red}`,
+    boxShadow: `0 0 ${sizes[1]} ${colors.blue}`,
   },
   '& .hamburgerLine': {
     position: 'absolute',
@@ -40,8 +39,9 @@ const hamburger = css({
     left: 0,
     margin: 'auto',
     width: '18px',
-    height: '2px',
-    backgroundColor: '#f9d8ae',
+    height: sizes[1],
+    borderRadius: sizes[1],
+    backgroundColor: colors.yellow,
     WebkitTransition: 'inherit',
     transition: 'inherit',
   },
@@ -50,37 +50,41 @@ const hamburger = css({
     display: 'block',
     width: '100%',
     height: '100%',
-    backgroundColor: 'inherit',
+    borderRadius: sizes[1],
+    backgroundColor: colors.lightBlue,
     content: "''",
     WebkitTransition: 'inherit',
     transition: 'inherit',
-    top: '-5px',
+    top: '-8px',
   },
   '& .hamburgerLine::after': {
     position: 'absolute',
     display: 'block',
     width: '100%',
     height: '100%',
-    backgroundColor: 'inherit',
+    borderRadius: sizes[1],
+    backgroundColor: colors.red,
     content: "''",
     WebkitTransition: 'inherit',
     transition: 'inherit',
-    top: '5px',
+    top: '8px',
   },
   "& .hamburger[aria-expanded='true']": {
+    border: `${sizes[1]} solid ${colors.yellow}`,
+    boxShadow: `0 0 ${sizes[1]} ${colors.yellow}`,
     '& .hamburgerLine': {
       backgroundColor: 'transparent',
     },
     '& .hamburgerLine::before': {
       top: 0,
-      backgroundColor: '#f9d8ae',
+      backgroundColor: colors.lightBlue,
       WebkitTransform: 'rotate(45deg)',
       '-ms-transform': 'rotate(45deg)',
       transform: 'rotate(45deg)',
     },
     '& .hamburgerLine::after': {
       top: 0,
-      backgroundColor: '#f9d8ae',
+      backgroundColor: colors.red,
       WebkitTransform: 'rotate(-45deg)',
       '-ms-transform': 'rotate(-45deg)',
       transform: 'rotate(-45deg)',
@@ -101,13 +105,16 @@ const hamburger = css({
 });
 
 export const Burger = () => {
+  const [ariaExpanded, setAriaExpanded] = useState(false);
+
   return (
     <div css={hamburger}>
       <button
         type="button"
         className="button hamburger"
         aria-controls="global-nav"
-        aria-expanded="false"
+        aria-expanded={ariaExpanded}
+        onClick={() => setAriaExpanded(!ariaExpanded)}
       >
         <span className="hamburgerLine">
           <span className="visuallyHidden">
