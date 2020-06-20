688e2c35948d215026bc8a7d07a43bc963578865
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-06-20 17:49:10 +0900

    エラーページを作成

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
+import Link from 'gatsby-link';
+import { FluidImage } from '../lib/images';
 import { SEO } from '../components/templates/Seo';
+import { css } from '@emotion/core';
+import { colors } from '../theme';
 
 type Props = {
   location: Location;
 };
+
+const ErrorPageContainer = css({
+  margin: '0 auto',
+  '& h1': {
+    color: colors.blue,
+  },
+  '& .ErrorPageText': {
+    margin: '0 auto',
+    textAlign: 'center',
+    '& a': {
+      textDecoration: 'none',
+      color: colors.blue,
+    },
+    '& a:hover': {
+      color: colors.red,
+    },
+  },
+});
 
 const NotFoundPage: React.FC<Props> = ({ location }) => (
   <>
@@ -11,11 +33,17 @@ const NotFoundPage: React.FC<Props> = ({ location }) => (
       pagetitle="404エラーページ"
       pagepath={location.pathname}
     />
-    <h1>NOT FOUND</h1>
-    <p>
-      You just hit a route that doesn&#39;t exist... the
-      sadness.
-    </p>
+    <div css={ErrorPageContainer}>
+      <h1>404</h1>
+      <FluidImage
+        filename="404.png"
+        alt="エラーページの画像"
+      />
+      <div className="ErrorPageText">
+        <p>ごめんなさい！ページが見つかりません</p>
+        <Link to="/">HOMEに戻る</Link>
+      </div>
+    </div>
   </>
 );
 
