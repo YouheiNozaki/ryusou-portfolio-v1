3ee9176158b36976e3e504883d013edcd1940d28
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-06-25 22:32:46 +0900

    gatsby-browser.jsをimportに変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
-const React = require('react');
-const Layout = require('./src/components/layout');
+import React from 'react';
+import Layout from './src/components/layout';
 
-require(`./node_modules/highlight.js/styles/a11y-light.css`);
+import(`./node_modules/highlight.js/styles/a11y-light.css`);
 
-module.exports = wrapPageElement = ({ element }) => {
+export default wrapPageElement = ({ element }) => {
   return <Layout>{element}</Layout>;
 };
