7f3268d3362274b6201f944739c10527177202e8
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-09-01 22:26:57 +0900

    codeハイライトのテーマを変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Layout } from './src/components/layout';
 
-import(`./node_modules/highlight.js/styles/a11y-light.css`);
+import(`./node_modules/highlight.js/styles/night-owl.css`);
 
 export const wrapPageElement = ({ element }) => {
   return <Layout>{element}</Layout>;
